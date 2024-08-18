aaaf036b16c854b89e7e3715d840ad039724d161
FileXDude <omehomechannel@gmail.com>
2024-08-18 15:13:50 +0700

    Create cbb.py

---
+++ plugins/cbb.py
@@ -0,0 +1,30 @@
+#(©)Codexbotz
+
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>ғɪʟᴇxʙᴏᴛ</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/filesharingdude'>ғɪʟᴇ sʜᴀʀɪɴɢ ᴅᴜᴅᴇ | ғɪʟᴇ x ᴅᴜᴅᴇ</a></b>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                    InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data = "close"),
+                    InlineKeyboardButton('ɢɪᴛʜᴜʙ', url='https://github.com/filexdude/')
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
+          
